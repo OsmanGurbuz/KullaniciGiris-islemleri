9db15b68051fbd9256287e90c102857f9f55547c
OsmanGurbuz <107862900+OsmanGurbuz@users.noreply.github.com>
2022-06-30 14:46:05 +0300

    Kullanıcı Giriş işlemleri
    Kullanıcı Giriş işlemleri

---
+++ KullanıcıGirisİşlemleri/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanıcıGirisİşlemleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kadı = "admin", ksifre = "123";
+            string kullaniciad, kullanicisifre;
+            int hak = 3;
+            do
+            {
+                Console.WriteLine("Kullsnıcı adını giriniz");
+                kullaniciad = Console.ReadLine();
+                Console.WriteLine("Şifrenizi giriniz");
+                kullanicisifre = Console.ReadLine();
+
+                if (kullaniciad == kadı && kullanicisifre == ksifre)
+                {
+                    Console.WriteLine("HOŞGELDİNİZ");
+                }
+                else
+                {
+                    hak--;
+                    if (hak == 0)
+                    {
+                        Console.Write("Üç defa doğru Kullanıcı adı ve Şifre giremediniz !");
+                    }
+                }
+            } while ((kullaniciad!= kadı || kullanicisifre!= ksifre)&& hak!= 0);
+
+            Console.ReadLine();
+        }
+
+    }
+}
